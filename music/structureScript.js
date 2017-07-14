0653a08bca150b1d0d56018a65aa7fdadc6bf287
TheSheepGuy <TheSheepGuy1@gmail.com>
2017-07-14 22:03:43 +0200

    Added 'strucutre' page
    And made other minor edits to other pages.

---
+++ music/structureScript.js
@@ -0,0 +1,212 @@
+function pathetiqueControl(task){
+    var piece = document.getElementById("pathetique");
+    
+    if(task == "expo1"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "expo2"){
+        piece.currentTime = 126.65; // = 2 min 6 s 650 ms
+        piece.play();
+    }
+    else if(task == "develop"){
+        piece.currentTime = 199.8; // = 3 min 19 s 800 ms
+        piece.play();
+    }
+    else if(task == "recap1"){
+        piece.currentTime = 304.25; // = 5 min 4 s 250 ms
+        piece.play();
+    }
+    else if(task == "recap2"){
+        piece.currentTime = 373.8; // = 6 min 13 s 800 ms
+        piece.play();
+    }
+}
+function funeralControl(task){
+    var piece = document.getElementById("funeral");
+    
+    if(task == "expo1"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "expo2"){
+        piece.currentTime = 54.75; // = 54 s 750 ms
+        piece.play();
+    }
+    else if(task == "develop"){
+        piece.currentTime = 268; // = 4 min 28 s
+        piece.play();
+    }
+    else if(task == "recap1"){
+        piece.currentTime = 363.1; // = 6 min 3 s 100 ms
+        piece.play();
+    }
+}
+function moonlightControl(task){
+    var piece = document.getElementById("moonlight");
+    
+    if(task == "expo1"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "expo2"){
+        piece.currentTime = 30.2; // = 30 s 200 ms
+        piece.play();
+    }
+    else if(task == "develop"){
+        piece.currentTime = 193.5; // = 3 min 13 s 800 ms
+        piece.play();
+    }
+    else if(task == "recap1"){
+        piece.currentTime = 250.25; // = 4 min 10 s 250 ms
+        piece.play();
+    }
+    else if(task == "recap2"){
+        piece.currentTime = 272; // = 4 min 32 s
+        piece.play();
+    }
+}
+
+//Rondo Form:
+function pathetique3Control(task){
+    var piece = document.getElementById("pathetique3")
+
+    if(task == "a1"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "b1"){
+        piece.currentTime = 22.5; // = 0 min 22 s 5 ms
+        piece.play();
+    }
+    else if(task == "a2"){
+        piece.currentTime = 79.5; // = 1 min 19 s
+        piece.play();
+    }
+    else if(task == "c"){
+        piece.currentTime = 101.5; // = 1 min 41 s
+        piece.play();
+    }
+    else if(task == "a3"){
+        piece.currentTime = 155.8; // = 2 min 35 s 800 ms
+        piece.play();
+    }
+    else if(task == "b2"){
+        piece.currentTime = 165.75; // = 2 min 45 s 750 ms
+        piece.play();
+    }
+    else if(task == "a4"){
+        piece.currentTime = 220; // = 3 min 40 s
+        piece.play();
+    }
+    else if(task == "e"){
+        piece.currentTime = 229; // = 3 min 49 s
+        piece.play();
+    }
+}
+
+//Sonata Rondo Form:
+function tempestControl(task){
+    var piece = document.getElementById("tempest");
+    
+    if(task == "expo1"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "expo2"){
+        piece.currentTime = 53.6; // = 53 s 6 ms
+        piece.play();
+    }
+    else if(task == "develop1"){
+        piece.currentTime = 77; // = 3 min 13 s 800 ms
+        piece.play();
+    }
+    else if(task == "develop2"){
+        piece.currentTime = 232; // = 3 min 58 s
+        piece.play();
+    }
+    else if(task == "recap1"){
+        piece.currentTime = 276; // = 4 min 10 s 250 ms
+        piece.play();
+    }
+    else if(task == "recap2"){
+        piece.currentTime = 272; // = 4 min 32 s
+        piece.play();
+    }
+}
+
+//Variations:
+function mozartVariationsControl(task){
+    var piece = document.getElementById("mozartVariations");
+    
+    if(task == "theme"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "I"){
+        piece.currentTime = 43;
+        piece.play();
+    }
+    else if(task == "II"){
+        piece.currentTime = 88;
+        piece.play();
+    }
+    else if(task == "III"){
+        piece.currentTime = 131.5; //!!! = 2 min 12 s !!!
+        piece.play();
+    }
+    else if(task == "IV"){
+        piece.currentTime = 175.5; // = 2 min 56 s
+        piece.play();
+    }
+    else if(task == "V"){
+        piece.currentTime = 218; // = 3 min 38 s
+        piece.play();
+    }
+    else if(task == "VI"){
+        piece.currentTime = 254; // = 4 min 14 s
+        piece.play();
+    }
+    else if(task == "VII"){
+        piece.currentTime = 296; // = 4 min 56 s
+        piece.play();
+    }
+    else if(task == "VIII"){
+        piece.currentTime = 338; // = 5 min 38 s
+        piece.play();
+    }
+    else if(task == "IX"){
+        piece.currentTime = 384; // = 6 min 24 s
+        piece.play();
+    }
+    else if(task == "X"){
+        piece.currentTime = 426; // = 7 min 6 s
+        piece.play();
+    }
+    else if(task == "XI"){
+        piece.currentTime = 465.5; // = 7 min 46 s
+        piece.play();
+    }
+    else if(task == "XII"){
+        piece.currentTime = 612.5; // = 10 min 13 s
+        piece.play();
+    }
+}
+
+//Ternary:
+function tristesseControl(task){
+    var piece = document.getElementById("tristesse");
+    
+    if(task == "a1"){
+        piece.currentTime = 0;
+        piece.play();
+    }
+    else if(task == "b"){
+        piece.currentTime = 95; // = 1 min 35 s
+        piece.play();
+    }
+    else if(task == "a2"){
+        piece.currentTime = 201; // = 3 min 21 s
+        piece.play();
+    }
+}
