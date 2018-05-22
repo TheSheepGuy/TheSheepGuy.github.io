47161fed0caa2238db375fbd5bd0359aa3481886
TheSheepGuy <TheSheepGuy1@gmail.com>
2018-05-22 19:39:31 +0100

    Converted Tabs to Spaces and Added How To Work Out Intervals

--- music/intervals.js
+++ music/intervals.js
@@ -3,26 +3,26 @@ const synth = new Tone.PolySynth(8, Tone.Synth);
 synth.toMaster();
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 //Get the buttons in the table:
 let tableButtons = document.getElementsByClassName("tableButtons");
 
 for (let n = 0; n < tableButtons.length; n++) {
-    tableButtons[n].addEventListener("click", () => {
-        playIntervalFromC(n);
-    }, false);
+	tableButtons[n].addEventListener("click", () => {
+		playIntervalFromC(n);
+	}, false);
 }
 
 async function playIntervalFromC(semitones) {
-    let intervalNotes = ["C4", "Db4", "D4", "Eb4", "E4", "F4", "F#4", "G4", "Ab4", "A4", "Bb4", "B4", "C5"];
+	let intervalNotes = ["C4", "Db4", "D4", "Eb4", "E4", "F4", "F#4", "G4", "Ab4", "A4", "Bb4", "B4", "C5"];
 
-    synth.triggerAttackRelease(["C4", intervalNotes[semitones]], "2s");
+	synth.triggerAttackRelease(["C4", intervalNotes[semitones]], "2s");
 }
 
 //Table:
 
 document.getElementById("tableCollapseButton").addEventListener("click", () => {
-    document.getElementById("intervalTable").classList.toggle("hidden");
+	document.getElementById("intervalTable").classList.toggle("hidden");
 }, false);
